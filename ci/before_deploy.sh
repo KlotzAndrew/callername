066f622977619486f9e075cda27ffecaf186676e
Andrew Klotz <agc.klotz@gmail.com>
2020-01-05 22:36:19 +0000

    typo in tar command

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -4,7 +4,7 @@ set -ex
 
 mkdir -p releases
 GOOS=linux GOARCH=amd64 go build -o ./releases/linux-amd64 .
-tar -czfv ./releases/linux-amd64.tar.gz ./releases/linux-amd64
+tar czfv ./releases/linux-amd64.tar.gz ./releases/linux-amd64
 
 shasum -a 512 ./releases/* > ./releases/sha512sum.txt
 cat ./releases/sha512sum.txt
